806401a0f23497c30475217576598305361db594
Ali AYÇİÇEK <aliaycicek_7010@hotmail.com>
2024-07-12 23:59:28 +0300

    First Commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicInstaClone
+//
+//  Created by Ali Ayçiçek on 12.07.2024.
+//
+
+import UIKit
+import FirebaseCore
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
